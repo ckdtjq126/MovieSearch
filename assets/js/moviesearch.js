31e206706a5f00f941f5846e22ae533f4e002f73
Chris Lee <ckdtjq126@gmail.com>
2015-04-05 23:22:36 -0400

    added api

--- assets/js/moviesearch.js
+++ assets/js/moviesearch.js
@@ -1,4 +1,11 @@
 var movie_id;
+var key= 'd957d3eec5eeec21e215b88e2b4f9964';
+var baseURL = 'http://api.themoviedb.org/3/search/movie';
+var searchURL = 'http://api.themoviedb.org/3/movie/';
+var apiURL = '?api_key=' + key;
+var queryURL = '&query=';
+var finalURL = baseURL + apiURL + queryURL;
+
 
 (function(){
   movie_id = window.location.search.substring(1).slice(0);
@@ -9,11 +16,29 @@ $(document).ready(function(){
   if(movie_id === ''){
     indexPage();
   } else {
-    renderMovie(movie_id);
-    /*getMovie(movie_id);*/
+    checkError();
   }
 });
 
+var checkError = function(){
+  var checkURL = finalURL + encodeURI(movie_id);
+  $.ajax({
+    url: checkURL,
+    async: false,
+    dataType: 'json',
+    success: function(data){
+      if(data.total_results === 0) {
+        errorPage();
+      } else {
+        renderMovie(data);
+      }
+    },
+    error: function(data){
+      errorPage();
+    }
+
+  });
+};
 
 var indexPage = function(){
   $.ajax({
@@ -26,29 +51,51 @@ var indexPage = function(){
   });
 };
 
-var getMovie = function(movie_id){
-  var url = "https://www.themoviedb.org/search?query="+movie_id;
+var renderMovie = function(data){
   $.ajax({
-    url: url,
-    async: false,
-    dataType: 'json',
-    success: function(data){
-      renderMovie(data);},
-    error: function(data){
-      pageNotFound();
-    }
-  });
-};
-
-var renderMovie = function(movie_id){
-  $.ajax({
-    url = "https://www.themoviedb.org/search?query="+movie_id;
+    url = 'views/show.html',
     dataType: 'html',
     success: function(response){
       var view, template, html;
       $('#moviesearch').html('');
       template = response;
       html = Mustache.to_html(response, movie_id);
+      $('#moviesearch').append($(html));
+
+      $('.choose').click(function(){
+        var id = $(this).children().attr('alt');
+        var idIndex = id.indexOf(':');
+        id = id.substring(0,idIndex);
+        getMovie(id);
+      });
+    }
+  });
+};
+
+var getMovie = function(movie_id){
+  var resultURL = searchURL + movie_id + apiURL;
+  $.ajax({
+    url: url,
+    async: false,
+    dataType: 'json',
+    success: function(data){
+      detail(data);
+    },
+    error: function(data){
+      pageNotFound();
+    }
+  });
+};
+
+var detail = function(data){
+  $.ajax({
+    url: 'views/detail.html',
+    dataType: 'html',
+    success : function(res){
+      var template, html;
+      $('#moviesearch').html('');
+      template = res;
+      html = Mustache.to_html(res, data);
       $('#moviesearch').append($(html));
     }
   });
